05ed698c620ae510a9bfb5f41b4918fbec3a6f96
Aya Okayama <r8aya8mg@gmail.com>
2013-07-11 19:18:19 +0000

    Updated web.js A3P1-7

--- web.js
+++ web.js
@@ -3,12 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 
-var buf = new Buffer(fs.readFileSync("index.html"));
-//buf.toString("utf8");
-
-
 app.get('/', function(request, response) {
-  response.send(buf.toString("utf8"));
+  var buf = new Buffer();
+  response.send(buf.toString('utf8', fs.readFileSync("index.html"));
 });
 
 var port = process.env.PORT || 5000;
