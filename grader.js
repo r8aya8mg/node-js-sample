6d83bc3e979f8d27763953adfdbe73cc76dab057
Aya Okayama <r8aya8mg@gmail.com>
2013-07-12 01:04:35 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -26,7 +26,19 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+//////////////
+var CHECKSURL_DEFAULT = "http://pacific-bastion-4335.herokuapp.com/";
+var util = require('util');//Aya
+var rest = require('restler')//Aya
 
+var urlcheck = rest.get('http://pacific-bastion-4335.herokuapp.com/').on('complete',function(result){
+    fs.writeFile('someURL.html',result);
+    //console.log(result);
+});
+
+
+
+//////////////
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -65,6 +77,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --url <web_page>', 'Path to checks.json', clone(assertFileExists), CHECKSURL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
